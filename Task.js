dc0de386643c8f37e2767ae7294fe84aa80f58da
kamile-v <157083698+kamile-v@users.noreply.github.com>
2024-06-06 12:30:07 -0700

    Update Task.js

--- Task.js
+++ Task.js
@@ -1,5 +1,16 @@
+/*
+ * Team 3:
+ * Tracy Mai
+ * Minnie Cao
+ * Kamile Vaicekonis
+ * CSC3221 - Netcentric Computing
+ * Task Manager Project
+ * Task.js
+ */
+
 const mongoose = require("mongoose");
 
+// This function creates a new task schema using Mongoose and defines the structure of the document.
 const TaskSchema = new mongoose.Schema({
     name:{
         type: String,
